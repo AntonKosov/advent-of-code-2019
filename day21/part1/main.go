dd8ba5e578eb7e801bd2ceaed378627dd38cb2e5
Anton Kosov <anton@kosov.dev>
2025-06-14 08:26:20 -0500

    day 21

---
+++ day21/part1/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/AntonKosov/advent-of-code-2019/day15/part1/program"
+)
+
+func main() {
+	run(os.Stdin, os.Stdout)
+	fmt.Println()
+}
+
+func run(reader io.Reader, writer io.Writer) {
+	inputData := read(reader)
+	answer := process(inputData)
+	fmt.Fprint(writer, answer)
+}
+
+func read(reader io.Reader) []int64 {
+	return program.Parse(reader)
+}
+
+// @ABCD
+// ^...^
+// ^...#
+// ^#..^
+//
+// !A || ((!B || !C) && D)
+const springscript = `NOT B T
+NOT C J
+OR T J
+AND D J
+NOT A T
+OR T J
+WALK
+`
+
+func process(code []int64) int64 {
+	input := make(chan int64, len(springscript))
+	defer close(input)
+	for _, v := range springscript {
+		input <- int64(v)
+	}
+
+	output := make(chan int64)
+	defer close(output)
+
+	var damage int64
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range output {
+			if v > 0xff {
+				damage = v
+				return
+			}
+			fmt.Print(string(rune(v)))
+		}
+	}()
+
+	program.Run(context.Background(), code, input, output)
+
+	wg.Wait()
+
+	return damage
+}
---
+++ day21/part2/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/AntonKosov/advent-of-code-2019/day15/part1/program"
+)
+
+func main() {
+	run(os.Stdin, os.Stdout)
+	fmt.Println()
+}
+
+func run(reader io.Reader, writer io.Writer) {
+	inputData := read(reader)
+	answer := process(inputData)
+	fmt.Fprint(writer, answer)
+}
+
+func read(reader io.Reader) []int64 {
+	return program.Parse(reader)
+}
+
+// @ABCDEFGHI
+// ^...^...##
+// ^...#^...#
+// ^#..^##.^.
+// Don't jump:
+// ^###^..###.#..##
+// ^#.#^.##..
+//
+// !A || ((!B || !C) && D && H)
+const springscript = `NOT B T
+NOT C J
+OR T J
+AND D J
+AND H J
+NOT A T
+OR T J
+RUN
+`
+
+func process(code []int64) int64 {
+	input := make(chan int64, len(springscript))
+	defer close(input)
+	for _, v := range springscript {
+		input <- int64(v)
+	}
+
+	output := make(chan int64)
+	defer close(output)
+
+	var damage int64
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range output {
+			if v > 0xff {
+				damage = v
+				return
+			}
+			fmt.Print(string(rune(v)))
+		}
+	}()
+
+	program.Run(context.Background(), code, input, output)
+
+	wg.Wait()
+
+	return damage
+}
